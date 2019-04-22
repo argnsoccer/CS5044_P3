08cc08ff937ea9af5718f2fe66a2beffba6f6851
argnsoccer <amgras@smu.edu>
2019-04-22 11:20:04 +0100

    lenght findin

--- scripts/line.js
+++ scripts/line.js
@@ -21,7 +21,7 @@ class LineChart
             return d.id
         })
         .entries(opsData);
-        var length = d3.values(missionWeatherData[0]);
+        var length = missionWeatherData[0].length;
         console.log(missionWeatherData[0])
         console.log(length);
         console.log(missionWeatherData);
